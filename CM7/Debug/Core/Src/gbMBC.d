24757cf26581f9539dc68eef1bc2ceed8c942b42
rmsaad <swift_gravy@hotmail.com>
2021-07-27 23:01:13 -0400

    removed some files

--- CM7/Debug/Core/Src/gbMBC.d
+++
@@ -1,3 +0,0 @@
-Core/Src/gbMBC.o: ../Core/Src/gbMBC.c ../Core/Inc/gbmemory.h
-
-../Core/Inc/gbmemory.h:
--- CM7/Debug/Core/Src/gbfunctions.d
+++
@@ -1,6 +0,0 @@
-Core/Src/gbfunctions.o: ../Core/Src/gbfunctions.c \
- ../Core/Inc/gbfunctions.h ../Core/Inc/gbmemory.h
-
-../Core/Inc/gbfunctions.h:
-
-../Core/Inc/gbmemory.h:
